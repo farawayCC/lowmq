a058127e57cba550a067f8348457131efa441673
Alexey Klygin <iosdevfarconsoft@gmail.com>
2022-10-16 10:05:08 +0200

    Changed the release folder from 'version-number' to 'lowmq-latest'
    This is handy for deploying in production. See readme/Usage/Production

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,12 +2,6 @@
 #!/bin/bash
 # This script prepares a release of the project.
 
-# Check if version is specified
-if [ -z "$1" ]; then
-    echo "Usage: prepare-release.sh <version>"
-    exit 1
-fi
-
 # Check if the script is run from the root of the project.
 if [ ! -f "prepare-release.sh" ]; then
     echo "This script must be run from the root of the project."
@@ -42,33 +36,33 @@ npm run build
 rm -rf node_modules/typescript node_modules/@types mocha
 
 # Make sure release directory is clean.
-if [ -d "build/Release/$1" ]; then
-    rm -rf build/Release/$1
+if [ -d "build/Release/lowmq-latest" ]; then
+    rm -rf build/Release/lowmq-latest
 fi
 
 # Create a folder for the release
-mkdir build/Release/$1
+mkdir build/Release/lowmq-latest
 
 # Copy the package.json to the release folder
-cp package.json build/Release/$1
+cp package.json build/Release/lowmq-latest
 
 # Copy the build files to the release folder
-cp -R dist build/Release/$1
+cp -R dist build/Release/lowmq-latest
 
 # Copy node_modules to the release folder
-cp -R node_modules build/Release/$1
+cp -R node_modules build/Release/lowmq-latest
 
 # Copy resources to the release folder
-cp -R resources build/Release/$1
+cp -R resources build/Release/lowmq-latest
 
 # Create a zip file for the release
 rm -f build/Release/lowmq.zip
 cd build/Release
-zip -rq lowmq.zip $1
+zip -rq lowmq.zip lowmq-latest
 
 
 # Regenerate the node_modules folder
 npm ci
 
 # Print that the release is ready
-echo "Release $1 is ready."
+echo "Release is ready."
