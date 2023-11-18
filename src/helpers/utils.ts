b99ac858d53290046b82e26ddbaf5392254ab014
Alexey Klygin <iosdevfarconsoft@gmail.com>
2023-11-18 17:56:28 +0100

    Handle empty db on start

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -4,10 +4,16 @@ import config, { rootPath } from "../config.js";
 
 
 export const checkDefaults = () => {
-    // default tokens
+    // Check if tokens file exists
     const pathToTokensFile = join(rootPath, 'resources', 'tokens'); pathToTokensFile
     if (!fs.existsSync(pathToTokensFile))
         fs.writeFileSync(pathToTokensFile, config.defaultPassword, 'utf-8');
+    // Warn if default password is still in use
     else if (fs.readFileSync(pathToTokensFile, 'utf-8').includes(config.defaultPassword))
         console.warn('Default password is still in use. Please change it in resources/tokens');
+
+    // Check if db file exists
+    const emptyDB = '{}'
+    if (!fs.existsSync(config.dbFilePath))
+        fs.writeFileSync(config.dbFilePath, emptyDB, 'utf-8')
 }
